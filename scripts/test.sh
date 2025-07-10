c244e9c003312f20ffe3fe653f99e1ace7a1c2e2
Jesus Zambrano <jgzambrano99@gmail.com>
2025-07-09 19:39:52 -0700

    feat: enhance test script with Postgres check and formatting improvements

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
 echo "🧪 Ejecutando pruebas con Pytest..."
-cd backend
-if [ -f "env/Scripts/activate" ]; then
-	source env/Scripts/activate
-elif [ -f "env/bin/activate" ]; then
-	source env/bin/activate
-else
-	echo "No se encontró el entorno virtual. Por favor, crea uno en 'env'."
-	exit 1
+
+# Verifica que Postgres esté escuchando en localhost:5432
+echo "🔍 Verificando que Postgres esté corriendo en localhost:5432..."
+if ! nc -z localhost 5432; then
+  echo "❌ No se detectó Postgres en localhost:5432."
+  echo "Por favor, inicia tu contenedor con Docker antes de correr los tests."
+  echo "🟢 Comando que inicia el contenedor en local: './scripts/start.sh'"
+  exit 1
 fi
+
+cd backend
+
+if [ -f "env/Scripts/activate" ]; then
+  source env/Scripts/activate
+elif [ -f "env/bin/activate" ]; then
+  source env/bin/activate
+else
+  echo "No se encontró el entorno virtual. Por favor, crea uno en 'env'."
+  exit 1
+fi
+
 PYTHONPATH=$(pwd) pytest app/tests/
